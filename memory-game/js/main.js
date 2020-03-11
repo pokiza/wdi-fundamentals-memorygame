8320be61a09a0bd77517d673e7aa2f4b3a697065
Pokiza Sobirova <pokizasobirova@Pokizas-Air.home>
2020-03-10 22:21:08 -0400

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,7 +1,30 @@
-console.log("Up and running!");
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree ="king";
-let cardFour ="king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+// console.log("Up and running!");
+// let cardOne = "queen";
+// let cardTwo = "queen";
+// let cardThree ="king";
+// let cardFour ="king";
+ console.log("User flipped queen" );
+ console.log("User flipped king" );
+
+
+var cards = ["Queen", "Queen", "King", "King"];
+
+var cardsInPlay = [];
+
+
+
+var cardOne = cards[0];
+var cardTwo = cards[2];
+var cardThree = [];
+var cardFour = [];
+
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+ if (cardsInPlay.length === 2)
+  { alert("Sorry, try again");
+   if (cardsInPlay[0] === cardsInPlay[1])
+    alert("You found a match!"); 
+else alert("Sorry, try again"); } 
+
